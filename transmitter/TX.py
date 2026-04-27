f9eb13b470be632f83532fbb3887493a2c784bd7
PEN <uemasaki20011222@gmail.com>
2026-04-27 19:21:20 +0900

    20260427

---
+++ transmitter/TX.py
@@ -0,0 +1,9 @@
+import socket
+
+if __name__ == "__main__":
+    ip = "192.168.10.65"
+    port = 8080
+    while True:
+        txData = input("wasd : ")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(txData[0].encode("UTF-8"), (ip, port))
