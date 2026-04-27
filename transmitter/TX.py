64f31e31c96c4a5f9c399a262e81beac4056549f
PEN <uemasaki20011222@gmail.com>
2026-04-27 20:45:04 +0900

    TX

--- transmitter/TX.py
+++ transmitter/TX.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     ip = "192.168.10.60"
     port = 8080
     while True:
-        txData = input("wasd : ")
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(txData[0].encode("UTF-8"), (ip, port))
+        data    = input("wasd : ")
+        txData  = data[0] if data else " "
+        sock    = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(txData.encode("UTF-8"), (ip, port))
